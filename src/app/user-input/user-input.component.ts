7f95560aa28afe07ad0ed8835d3f2e78a435b009
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-16 11:44:23 +0530

    User input working, minor issue solved

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
   styleUrls: ['./user-input.component.css']
 })
-export class UserInputComponent implements OnInit {
+export class UserInputComponent {
 
   num : number;
-  onSubmitEventEmitter = new EventEmitter<number>()
+  @Output() onSubmitEventEmitter = new EventEmitter<number>()
 
   constructor() { }
-
-  ngOnInit() {}
 
   onSubmit() {
     if (!this.num || this.num<=0) return
