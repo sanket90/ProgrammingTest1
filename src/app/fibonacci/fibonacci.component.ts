661e6f99bbebb375e0ff4d7aa5451d890309953f
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-16 11:53:22 +0530

    Fibonacci Series done

--- src/app/fibonacci/fibonacci.component.ts
+++ src/app/fibonacci/fibonacci.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-fibonacci',
   templateUrl: './fibonacci.component.html',
   styleUrls: ['./fibonacci.component.css']
 })
-export class FibonacciComponent implements OnInit {
+export class FibonacciComponent implements OnChanges {
 
   constructor() { }
 
-  ngOnInit() {
+  @Input() n : number;
+  fib : any
+
+  ngOnChanges() { this.fib = this.calcFibonacciSeries(this.n)}
+
+  private calcFibonacciSeries(num) {
+    let series = [];
+    for (let i=0; i<num; i++) {
+      if (i < 2) series[i]=1
+      else series[i]= series[i-1] + series[i-2]
+    }
+    return series
   }
 
 }
