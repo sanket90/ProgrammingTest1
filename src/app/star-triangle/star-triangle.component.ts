b3a9277d086c3d6f013c3be3c263b7a7cc28216f
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-16 12:08:33 +0530

    Star triangel done

--- src/app/star-triangle/star-triangle.component.ts
+++ src/app/star-triangle/star-triangle.component.ts
@@ -1,10 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-star-triangle',
   templateUrl: './star-triangle.component.html',
   styleUrls: ['./star-triangle.component.css']
 })
-export class StarTriangleComponent {
+export class StarTriangleComponent implements OnInit, OnChanges {
+
   @Input() n : any;
+  star : any;
+
+  ngOnInit() { this.star = this.generateStarTriangle(this.n) }
+
+  ngOnChanges() { this.star = this.generateStarTriangle(this.n) }
+  
+  private generateStarTriangle(num) {
+    let rows = []
+    for (let i=0; i<num; i++) {
+      let columns = []
+      for (let j=0; j<=i; j++) {
+        columns[j] = "* "
+      }
+      rows[i] = columns
+    }
+    return rows;
+  }
 }
